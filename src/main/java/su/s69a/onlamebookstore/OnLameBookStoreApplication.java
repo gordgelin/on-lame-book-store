98f7bfcb53e087a6c96afecaa012b86a4094c341
gordgelin <gordgelin@s69a.su>
2023-11-26 09:20:03 +0200

    Initial commit. Basic project configuration done. Added Spring Boot app template.

---
+++ src/main/java/su/s69a/onlamebookstore/OnLameBookStoreApplication.java
@@ -0,0 +1,13 @@
+package su.s69a.onlamebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnLameBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnLameBookStoreApplication.class, args);
+    }
+
+}
